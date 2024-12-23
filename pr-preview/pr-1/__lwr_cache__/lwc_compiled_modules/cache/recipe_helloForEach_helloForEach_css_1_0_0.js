342e224aaf7ec6462474648ef90b670029561366
Yoonsang Lee <yoonsang.lee@yoonsan-ltmn792.internal.salesforce.com>
2024-12-23 21:01:47 +0100

    removed preview

--- pr-preview/pr-1/__lwr_cache__/lwc_compiled_modules/cache/recipe_helloForEach_helloForEach_css_1_0_0.js
+++
@@ -1,8 +0,0 @@
-function stylesheet(token, useActualHostSelector, useNativeDirPseudoclass) {
-  var shadowSelector = token ? ("[" + token + "]") : "";
-  var hostSelector = token ? ("[" + token + "-host]") : "";
-  var suffixToken = token ? ("-" + token) : "";
-  return "ul" + shadowSelector + " {list-style: none;padding: 0;}";
-  /*LWC compiler v6.5.0*/
-}
-export default [stylesheet];
