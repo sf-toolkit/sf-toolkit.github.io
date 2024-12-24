dd0d28c04a0c75acfdd19fce3a2659e8687d9a25
Yoonsang Lee <yoonsang.lee@yoonsan-ltmn792.internal.salesforce.com>
2024-12-24 11:54:48 +0100

    remove preview3

--- pr-preview/pr-3/pr-preview/pr-2/__lwr_cache__/lwc_compiled_modules/cache/ui_navfooter_navfooter_css_0_3_2.js
+++
@@ -1,8 +0,0 @@
-function stylesheet(token, useActualHostSelector, useNativeDirPseudoclass) {
-  var shadowSelector = token ? ("[" + token + "]") : "";
-  var hostSelector = token ? ("[" + token + "-host]") : "";
-  var suffixToken = token ? ("-" + token) : "";
-  return "a" + shadowSelector + " {text-decoration: var(--text-decoration);color: var(--color-text-link);}.next" + shadowSelector + " {float: right;margin-right: 10px;}.previous" + shadowSelector + " {margin-left: 10px;}";
-  /*LWC compiler v6.5.0*/
-}
-export default [stylesheet];
