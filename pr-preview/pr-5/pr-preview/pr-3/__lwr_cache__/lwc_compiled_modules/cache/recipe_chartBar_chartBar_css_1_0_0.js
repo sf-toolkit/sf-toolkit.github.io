1e518de31434a522953c810d64f4e590b6ca7aaa
Yoonsang Lee <yoonsang.lee@yoonsan-ltmn792.internal.salesforce.com>
2024-12-24 11:51:29 +0100

    remove preview5

--- pr-preview/pr-5/pr-preview/pr-3/__lwr_cache__/lwc_compiled_modules/cache/recipe_chartBar_chartBar_css_1_0_0.js
+++
@@ -1,8 +0,0 @@
-function stylesheet(token, useActualHostSelector, useNativeDirPseudoclass) {
-  var shadowSelector = token ? ("[" + token + "]") : "";
-  var hostSelector = token ? ("[" + token + "-host]") : "";
-  var suffixToken = token ? ("-" + token) : "";
-  return ".container" + shadowSelector + " {overflow: hidden;color: #f5b041;display: flex;margin-top: 6px;}.text" + shadowSelector + " {margin-top: 5px;margin-left: 4px;}.bar" + shadowSelector + " {margin-left: 4px;height: 36px;background-color: #f5b041;}";
-  /*LWC compiler v6.5.0*/
-}
-export default [stylesheet];
